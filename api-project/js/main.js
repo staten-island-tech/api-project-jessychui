34d7461fc8a685c8d11873dd85f8f8e6450d2a0e
jessychui <jessy.chui@gmail.com>
2023-01-19 13:24:22 -0500

    yippee

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,3 +1,20 @@
 import "../style/style.css";
 
-smashdb.me / api / character;
+/* const nintendo = {
+  name: document.querySelector(),
+}; */
+
+let name = "kirby";
+
+async function getData(name) {
+  try {
+    const response = await fetch(
+      `https://www.amiiboapi.com/api/amiibo/?name=${name}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(name);
