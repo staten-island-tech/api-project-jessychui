1f1982464d6b191ae118a5e7bdf2a1eaacb4daab
jessychui <jessy.chui@gmail.com>
2023-01-31 14:04:39 -0500

    j

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -4,11 +4,11 @@ import "../style/style.css";
   name: document.querySelector(),
 }; */
 
-let name = "random";
+let name = "";
 
 async function getData(name) {
   try {
-    const response = await fetch(`https://dog.ceo/api/breeds/image/${name}`);
+    const response = await fetch(`https://www.thecolorapi.com/id?${name}`);
     const data = await response.json();
     console.log(data);
   } catch (error) {
