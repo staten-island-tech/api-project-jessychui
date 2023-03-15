c61d48406ed2cfbab58c6bd35a0f6edd9daf7370
jessychui <jessy.chui@gmail.com>
2023-03-15 13:56:31 -0400

    sss

--- api-project/main.js
+++ api-project/main.js
@@ -3,37 +3,40 @@ import "/style.css";
 const DOMselectors = {
   enter: document.getElementById("enter"),
   container: document.getElementById("container"),
-  number: document.getElementById("number")
-}
+  number: document.getElementById("number"),
+};
 
 const URL = "https://api.disneyapi.dev/characters";
 
 DOMselectors.container.innerHTML = "";
 
 async function getData() {
-    DOMselectors.container.innerHTML = "";
-    try{
-        const response = await fetch(URL); //
-        const data = await response.json();
-        Object.entries(data)
-        console.log(Object.entries(data));
-        let number = DOMselectors.number
-        Object.entries(data).filter((char) => char[1].number == `${"_id"}`)
-        .forEach(char => { 
-            console.log(char[1]["name"]);
-            DOMselectors.box.insertAdjacentHTML(
-                "beforeend",
-                `<div class="card">
-                <div class="name">${char[1]["name"].split("_").join(" ")}</div>
-                <img class="images" alt="${char[1]["name"]}" src="${char[1]["imageUrl"]}" />
+  DOMselectors.container.innerHTML = "";
+  try {
+    const response = await fetch(URL); //
+    const data = await response.json();
+    let realData = Object.entries(data)[0][1];
+    let number = Number(DOMselectors.number.value);
+    console.log(realData, number - 1);
+    let x = 0;
+    realData.map((thing) => {
+      thing._id = x;
+      x += 1;
+    });
+    realData
+      .filter((char) => number - 1 == char._id)
+      .forEach((char) => {
+        DOMselectors.container.insertAdjacentHTML(
+          "beforeend",
+          `<div class="card">
+                <div class="name">${char["name"]}</div>
+                <img class="images" alt="${char["name"]}" src="${char["imageUrl"]}" />
                 </div>`
-            );
-        });
-       
-        
-    } catch (error) {
-        console.log(error);
-    }
-    }
+        );
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 DOMselectors.enter.addEventListener("click", getData);
